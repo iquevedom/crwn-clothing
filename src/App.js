ce1a68eb91fced21a22ee643d22a710876ccd6cf
Isidro Quevedo <isidroquevedo@hotmail.com>
2019-07-20 17:06:30 -0700

    Sign in page and component

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import './App.css';
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component";
+import SignInAndSigUpPage from "./pages/signin-and-signup/signin-and-signup.component"
 
 /* 
 const HatsPage = () => (
@@ -21,6 +22,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
+        <Route path="/signin" component={SignInAndSigUpPage} />
       </Switch>
     </div>
   );
